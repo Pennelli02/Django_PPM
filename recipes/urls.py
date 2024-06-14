cbab5c90e333f5dc5c656923b686095c02d22c07
lorenzo pennelli <161378352+Pennelli02@users.noreply.github.com>
2024-06-14 12:34:42 +0200

    aggiunta urls per le ricette favorite

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('recipes/create/', RecipeCreateView.as_view(), name='recipesCreate'),
     path('recipes/<slug:slug>/edit/', RecipeUpdateView.as_view(), name='recipesEdit'),
     path('recipes/<slug:slug>/delete/', RecipeDeleteView.as_view(), name='recipesDelete'),
+    path('favourites/', recipe_views.favorite_recipes_list, name='favourites'),
     path('categories/', recipe_views.category_list, name='categories'),
     path('categories/<slug:slug>/', recipe_views.category_detail, name='categoryDetail'),
 ]
