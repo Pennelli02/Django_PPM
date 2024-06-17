3835d91ff9ae97d0e7c35975bc529c5f576c19b1
lorenzo pennelli <161378352+Pennelli02@users.noreply.github.com>
2024-06-17 10:23:11 +0200

    template per cancellare le ricette e visione dei like nelle ricette

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Count, Q
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
@@ -18,7 +19,7 @@ class RecipeListView(ListView):
 
 
 def HomeView(request):
-    mostLikedRecipes = Recipe.objects.annotate(likeCount=Count('likes')).order_by('-likes')[:4]
+    mostLikedRecipes = Recipe.objects.annotate(likeCount=Count('likes')).order_by('-likeCount')[:4]
     recentRecipes = Recipe.objects.order_by('-date_posted')[:4]
     return render(request, 'recipes/home.html', {'mostLiked': mostLikedRecipes, 'recent': recentRecipes})
 
@@ -90,21 +91,14 @@ class RecipeUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     form_class = RecipeForm
 
     def form_valid(self, form):
-        context = self.get_context_data()
-        ingredient_formset = context['ingredient_formset']
-        if form.is_valid() and ingredient_formset.is_valid():
-            self.object = form.save(commit=False)
-            self.object.author = self.request.user
-            self.object.save()
-            ingredient_formset.instance = self.object
-            ingredient_formset.save()
-            return redirect(self.get_success_url())
-        else:
-            return self.render_to_response(self.get_context_data(form=form))
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
     def test_func(self):
-        recipe = self.get_object()
-        return self.request.user == recipe.author
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 class RecipeDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
@@ -137,7 +131,8 @@ def addFavoriteRecipe(request, id):
     else:
         recipe.likes.add(user)
         messages.success(request, f'Recipe {recipe.title} has been added')
-    return redirect(recipe.get_absolute_url())
+    return HttpResponseRedirect(
+        recipe.get_absolute_url())  # reindirizza l'utente alla pagina dei dettagli della ricetta
 
 
 @login_required
@@ -146,15 +141,16 @@ def user_recipes_list(request):
 
 
 # mostra tutte le categorie presenti
-def category_list(request):
+def category_list(request, ):
     categories = Category.objects.all()
     return render(request, 'recipes/categories.html', {'categories': categories})
 
 
 # mostra le ricette presenti in una categoria
-def category_detail(request, pk):
-    categories = get_object_or_404(Category, pk=pk)
-    return render(request, 'recipes/RecipesForCategories.html', {"categories": categories})
+def category_detail(request, slug):
+    categories = get_object_or_404(Category, slug=slug)
+    recipes = Recipe.objects.filter(category=categories)
+    return render(request, 'recipes/RecipesForCategories.html', {"categories": categories, "recipes": recipes})
 
 
 # implementa la ricerca per categoria e titolo
