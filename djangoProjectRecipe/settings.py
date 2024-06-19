f5100b31335cd011aab087c919c0ccf9ee20fd29
lorenzo pennelli <161378352+Pennelli02@users.noreply.github.com>
2024-06-19 23:12:46 +0200

    uninstall bash

--- djangoProjectRecipe/settings.py
+++ djangoProjectRecipe/settings.py
@@ -17,7 +17,6 @@ from django.contrib import messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ SECRET_KEY = 'django-insecure-7gk!gh=n(=wl+2)88mkf9tjpea$w^ep*z_845=r2rpb=f!(lg=
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -76,19 +74,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoProjectRecipe.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://productiondatabase_9tjp_user:NlbbfhrbXioNuQ2cAP1hPayV7WFi1mMv@dpg-cppbuu5ds78s73e5kb9g-a'
-                ':5432/productiondatabase_9tjp',
-        conn_max_age=600
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+# DATABASES['default']=dj_database_url.parse('postgres://productiondatabase_9tjp_user:NlbbfhrbXioNuQ2cAP1hPayV7WFi1mMv@dpg-cppbuu5ds78s73e5kb9g-a.frankfurt-postgres.render.com/productiondatabase_9tjp')
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -107,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -120,7 +115,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
